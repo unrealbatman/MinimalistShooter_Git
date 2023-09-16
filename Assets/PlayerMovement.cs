52b075abd29046ecbb7ea76e6288198593855ff6
Rithviik Srinivasan <rithu6876@gmail.com>
2023-09-16 16:30:19 -0400

    no message

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float accelerationTime = 2f;
+    public float maxSpeed = 5f;
+    private Vector2 movement;
+    private float timeLeft;
+    public Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponentInParent<Rigidbody2D>();
+        Debug.Log(rb.name);
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        timeLeft -= Time.deltaTime;
+        if (timeLeft <= 0)
+        {
+            movement = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+            timeLeft += accelerationTime;
+        }
     }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(movement * maxSpeed);
+    }
+
+
+
 }
