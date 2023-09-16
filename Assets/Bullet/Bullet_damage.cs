562fe8868720484858e4cd76a8afb83f783bfc42
Justin Li <107675909+Justinyutingli@users.noreply.github.com>
2023-09-16 15:33:30 -0700

    Update the bullet Script

--- Assets/Bullet/Bullet_damage.cs
+++ Assets/Bullet/Bullet_damage.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class Bullet_damage : MonoBehaviour
 {
+    
+    [Range(1,10)]
+    [SerializeField] private float speed = 10f;
+    
+    [Range(1,10)]
+    [SerializeField] private float lifeTime = 3f;
+    
+    private Rigidbody2D rb;
+    
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 3f);
+        rb = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, lifeTime);
     }
 
+    ///*
+    private void FixedUpdate(){
+        rb.velocity = transform.up * speed;
+    }
+    // */
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<enemy>() != null){
